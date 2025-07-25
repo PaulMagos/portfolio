6228002cbb0a1149fc0bc6e149e2ca485cec552d
paulmagosION <paul.magos@iongroup.com>
2025-07-25 15:50:54 +0000

    Add dummy text

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,7 +4,7 @@ export const experiences = [
     title: 'Unknown',
     image: "/Unknown.png",
     company: "Unknown",
-    description: "Unknown",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     duration: "(Oct 2025)"
   },
   {
