b1e77bedc1fb317072c0ce26d48b5cdcb80eaca9
paulmagosION <paul.magos@iongroup.com>
2025-07-25 15:46:22 +0000

    Add new exp

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,27 @@
 export const educations = [
   {
     id: 1,
+    title: "Unknown",
+    duration: "2025 - ?",
+    image: "/Unknown.png",
+    institution: "Unknown",
+  },
+  {
+    id: 2,
     title: "Master Degree - Artificial Intelligence",
     duration: "2022 - 2024",
     image: "/UNIPI.png",
     institution: "University of Pisa",
   },
   {
-    id: 2,
+    id: 3,
     title: "Bachelor Degree - Computer Science",
     duration: "2019 - 2022",
     image: "/UNIPI.png",
     institution: "University of Pisa",
   },
   {
-    id: 3,
+    id: 4,
     title: "High School Diploma - Computer Science",
     duration: "2012 - 2018",
     image: "/ITIS.png",
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,22 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Unknown',
+    image: "/Unknown.png",
+    company: "Unknown",
+    description: "Unknown",
+    duration: "(Oct 2025)"
+  },
+  {
+    id: 2,
     title: 'Software & Artificial Intelligence Engineer',
     image: "/ION.png",
     company: "ION Trading Srl, Pisa, Italy",
     description: "Working on the backend of a financial product, making software architecture decisions and feature development in Java. Additionally, 20% of the time is allocated to a research project exploring applications of artificial intelligence in financial systems.",
-    duration: "(Feb 2025 - Present)"
+    duration: "(Feb 2025 - Aug 2025)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Research Fellow",
     image: "/UNIPI.png",
     company: "University of Pisa",
@@ -16,7 +24,7 @@ export const experiences = [
     duration: "(Jan 2024 - Jun 2024)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Freelance Software Engineer",
     image: "/PRIMIS.png",
     company: "PRIMIS Srl, Italy",
